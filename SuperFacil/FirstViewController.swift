39770e903d74f9452c03df2c5005067b5bd35edb
alau7 <alau7@gatech.edu>
2018-10-20 15:25:00 -0700

    Initial Commit

---
+++ SuperFacil/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SuperFacil
+//
+//  Created by Agustin Lau on 10/20/18.
+//  Copyright © 2018 Agustin A. Lau. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
