b721ce00059f5dae11c093e8cd43375bd2c1a72b
alau7 <alau7@gatech.edu>
2018-10-21 12:10:50 -0700

    Add first image and set up scroll view

--- SuperFacil/FirstViewController.swift
+++ SuperFacil/FirstViewController.swift
@@ -9,10 +9,26 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
+    
+    var names: [String] = ["Beverages", "Meat and Seafood", "Pantry"]
+    
+    @IBOutlet weak var produceButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Give the produce button an action
+        produceButton.addTarget(self, action: #selector(produceButtonAction), for: .touchUpInside)
+        
+        // Make produce button round
+        produceButton.layer.cornerRadius = produceButton.frame.size.width / 2
+        produceButton.clipsToBounds = true
+    
+    }
+    
+    @objc func produceButtonAction(sender: UIButton!) {
+        print("Button tapped")
     }
 
     override func didReceiveMemoryWarning() {
