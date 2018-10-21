ebd06ebd3b30cb117c963c2270831d90c5e22e88
alau7 <alau7@gatech.edu>
2018-10-21 13:00:02 -0700

    Add dairy button and label

--- SuperFacil/FirstViewController.swift
+++ SuperFacil/FirstViewController.swift
@@ -14,20 +14,32 @@ class FirstViewController: UIViewController {
     
     @IBOutlet weak var produceButton: UIButton!
     
+    @IBOutlet weak var meatSeafoodButton: UIButton!
+    
+    @IBOutlet weak var dairyButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        // Give the produce button an action
-        produceButton.addTarget(self, action: #selector(produceButtonAction), for: .touchUpInside)
+        // Give the all buttons an action
+        produceButton.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        meatSeafoodButton.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        dairyButton.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
         
-        // Make produce button round
+        // Make all buttons round
         produceButton.layer.cornerRadius = produceButton.frame.size.width / 2
         produceButton.clipsToBounds = true
+        
+        meatSeafoodButton.layer.cornerRadius = meatSeafoodButton.frame.size.width / 2
+        meatSeafoodButton.clipsToBounds = true
+        
+        dairyButton.layer.cornerRadius = produceButton.frame.size.width / 2
+        dairyButton.clipsToBounds = true
     
     }
     
-    @objc func produceButtonAction(sender: UIButton!) {
+    @objc func buttonAction(sender: UIButton!) {
         print("Button tapped")
     }
 
